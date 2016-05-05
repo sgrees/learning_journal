1f9661f5584cbf4eb0a52d73c35ce6712c67a269
Susan Rees <susan@reeshome.org>
2016-05-04 22:27:27 -0700

    added methods to Assignment 05

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -6,6 +6,8 @@ from sqlalchemy import (
     Unicode,
     UnicodeText,
     DateTime,
+    desc,
+    select,
     )
 
 from sqlalchemy.sql import func
@@ -27,10 +29,19 @@ class Entry(Base):
     id = Column(Integer, primary_key=True)
     title = Column(Unicode(255), nullable=False, unique=True)
     body = Column(UnicodeText())
-    created = Column(DateTime(timezone=True), default=func.now())
-    edited = Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
+    created = Column(DateTime(timezone=True), default=func.current_timestamp())
+    edited = Column(DateTime(timezone=True), default=func.current_timestamp(), onupdate=func.current_timestamp())
 
-# classmethod all that returns all the entries in the database, ordered so that the most recent entry is first.
-# classmethod by_id that returns a single entry, given an id.
+    def all(entries):
+        # return all entries in database
+        # ordered most recent entry first
+        ordered_entries = select([entries]).order_by(desc(entries.created)).all()
+        return ordered_entries
+
+    def by_id(entries, id):
+        # returns a single entry, given an id
+        unique_id = int(id)
+        find_unique_id = select([entries]).where(id=unique_id)
+        return find_unique_id
 
 Index('my_index', Entry.name, unique=True, mysql_length=255)
